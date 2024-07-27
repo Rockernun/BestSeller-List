976f6dd16f7f09606b81a84fb7cc0dc48d56cfae
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-07-27 19:32:32 +0900

    Books Lists 스타일링 작업

--- app/list/[id]/page.tsx
+++ app/list/[id]/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React from "react";
 import IBooks, { API_URL } from "../../utils/BooksInterface";
 import IBook from "../../utils/BooksInterface";
+import styles from "../../../styles/list.module.css";
 
 async function getBooks(id: string) {
   return fetch(`${API_URL}${id}`).then((response) => response.json());
@@ -10,21 +11,20 @@ async function getBooks(id: string) {
 export default async function Books({ params: { id } }) {
   const books: IBooks = await getBooks(id);
   return (
-    <div>
-      <h1>
-        <ul>
-          {books.results.books.map((book: IBook, index) => (
-            <li key={index}>
-              <img src={book.book_image} />
-              <span>{book.title}</span>
-              <span>{book.author}</span>
-              <Link href={book.amazon_product_url} target={"_blank"}>
-                <span>Buy Now! &rarr;</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </h1>
+    <div className={styles.container}>
+      <h1 className={styles.lists}>{books.results.list_name} List</h1>
+      <ul className={styles.list}>
+        {books.results.books.map((book: IBook, index) => (
+          <li className={styles.book} key={index}>
+            <img src={book.book_image} />
+            <span className={styles.title}>Title : {book.title}</span>
+            <span className={styles.author}>Author : {book.author}</span>
+            <Link href={book.amazon_product_url} target={"_blank"}>
+              <span className={styles.buy}> Buy Now! &rarr;</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
